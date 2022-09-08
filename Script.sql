3383b4ff0f44d259ca0b5e3e47c6d5f5a6758c79
stpntsar <stpncar@gmail.com>
2022-09-08 20:32:58 +0300

    sql

---
+++ Script.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS Genre (
+	genre_id SERIAL PRIMARY KEY,
+	name VARCHAR (60) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Performers (
+	performers_id SERIAL PRIMARY KEY,
+	name VARCHAR (60) NOT NULL	
+);
+CREATE TABLE IF NOT EXISTS Album (
+	album_id SERIAL PRIMARY KEY,
+	name VARCHAR (60) NOT NULL,
+	year DATE
+);
+CREATE TABLE IF NOT EXISTS Genre_performers (
+	genre_id INTEGER REFERENCES Genre (genre_id),
+	performers_id INTEGER REFERENCES Performers (performers_id)
+);
+CREATE TABLE IF NOT EXISTS Album_performers (
+	performers_id INTEGER REFERENCES Performers (performers_id),
+	album_id INTEGER REFERENCES Album (album_id)
+);
+CREATE TABLE IF NOT EXISTS Tracks (
+	tracks_id SERIAL PRIMARY KEY,
+	name VARCHAR (60) NOT NULL,
+	duration TIME,
+	album_id INTEGER REFERENCES Album (album_id)
+);
+CREATE TABLE IF NOT EXISTS Сollection (
+	collection_id SERIAL PRIMARY KEY,
+	name VARCHAR (60) NOT NULL,
+	year DATE
+);
+CREATE TABLE IF NOT EXISTS Collection_tracks (
+	collection_id INTEGER REFERENCES Сollection (collection_id),
+	tracks_id INTEGER REFERENCES Tracks (tracks_id)
+);
+	
